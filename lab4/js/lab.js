5f5a9eddcebf8e901a845e0fec873c39e88d3ced
Mppalmer1 <77123340+Mppalmer1@users.noreply.github.com>
2021-01-24 19:43:44 -0800

    new

---
+++ lab4/js/lab.js
@@ -0,0 +1,23 @@
+// Print the board before each turn
+   //add board
+    // add 7 rows
+     // add css
+      // add two colors to board
+       // customize two colors
+// Accept the user input
+   //open DOM
+    //create a representation of the board
+// Validate user input
+   //register user response
+// Evaluate the possible moves
+   //limit moves
+    //create possible moves
+     // create limits on what is allowed per players turn
+// Choose computer moves
+   //reaction after player moves, or before player moves
+    //left or right
+     //backwards once a piece reaches the other side of the board
+// Test for the win
+   //create variable that resets game
+    //create point system
+     //add points 0-9 
